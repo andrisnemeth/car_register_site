5e78d9691fa546f50b56dedec6017f6cb456090e
Németh Andris <n.andris939@gmail.com>
2023-06-30 00:52:07 +0200

    Add userContext, Provider to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { UserContext, UserProvider } from "./contexts/UserContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function RootComponent() {
+  const [currentUser, setCurrentUser] = React.useState({});
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <UserProvider value={{ currentUser, setCurrentUser }}>
+          <App />
+        </UserProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
+root.render(<RootComponent />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
