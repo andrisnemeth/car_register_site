fe4d3ba24e06aa327f963ca730fc78ead08f41c1
Németh Andris <n.andris939@gmail.com>
2023-07-03 10:42:44 +0200

    Update CarManagement with css and update registration.css

--- frontend/src/components/CarManagement.jsx
+++ frontend/src/components/CarManagement.jsx
@@ -8,9 +8,11 @@ import {
   addNewCarType,
 } from "../api/carManagement";
 import { validateBrandName } from "../helpers/inputFieldValidators";
+import "../styles/CarManagement.css";
 
 function CarManagement() {
   const [isAdmin, setIsAdmin] = useState(true);
+  const currentYear = new Date().getFullYear();
   //brand states
   const [brands, setBrands] = useState([]);
   const [selectedBrand, setSelectedBrand] = useState(0);
@@ -173,131 +175,148 @@ function CarManagement() {
 
   return (
     <>
-      <div>
-        <h1 style={{ textAlign: "center" }}>Autónyilvántartás</h1>
-        <h2>Profin, egyszerűen</h2>
-        {isAdmin && (
-          <>
-            <div>
+      <div className="car_data_form_container">
+        <h2 className="car_data_form_headline" style={{ textAlign: "center" }}>
+          Autónyilvántartás
+        </h2>
+        <h3>Profin, egyszerűen</h3>
+        <div className="car_data_form_content">
+          {isAdmin && (
+            <>
               <h4>Új márka hozzáadása</h4>
-              <Button size="md" rounded onPress={setAddNewBrandModalVisible}>
+              <Button
+                size="md"
+                rounded
+                onPress={setAddNewBrandModalVisible}
+                className="car_data_add_new_button"
+              >
                 Hozzáadás
               </Button>
-            </div>
-            <Modal
-              closeButton
-              blur
-              aria-labelledby="modal-title"
-              open={addNewBrandModalVisible}
-              onClose={closeModalHandler}
-            >
-              <Modal.Header>
-                <Text id="modal-title" size={18}>
-                  Új márkanév
-                </Text>
-              </Modal.Header>
-              <Modal.Body>
-                <Input
-                  clearable
-                  className={shakeBrandName ? "shake" : ""}
-                  onChange={(e) => setBrandName(e.target.value)}
-                  required
-                  bordered
-                  status={brandNameHelper.color}
-                  color={brandNameHelper.color}
-                  helperColor={brandNameHelper.color}
-                  helperText={brandNameHelper.text}
-                  fullWidth
-                  size="md"
-                  placeholder="Márkanév:"
-                  aria-labelledby="Márkanév"
-                />
-                <Spacer y={1} />
-              </Modal.Body>
-              <Modal.Footer>
-                <Button
-                  auto
-                  rounded
-                  flat
-                  color="error"
-                  onPress={closeModalHandler}
-                >
-                  Bezárás
-                </Button>
-                <Button auto rounded onPress={handleAddNewBrand}>
-                  Hozzáadás
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          </>
-        )}
+              <Modal
+                closeButton
+                blur
+                aria-labelledby="modal-title"
+                open={addNewBrandModalVisible}
+                onClose={closeModalHandler}
+              >
+                <Modal.Header>
+                  <Text id="modal-title" size={18}>
+                    Új márkanév
+                  </Text>
+                </Modal.Header>
+                <Modal.Body>
+                  <Input
+                    clearable
+                    className={shakeBrandName ? "shake" : ""}
+                    onChange={(e) => setBrandName(e.target.value)}
+                    required
+                    bordered
+                    status={brandNameHelper.color}
+                    color={brandNameHelper.color}
+                    helperColor={brandNameHelper.color}
+                    helperText={brandNameHelper.text}
+                    fullWidth
+                    size="md"
+                    placeholder="Márkanév:"
+                    aria-labelledby="Márkanév"
+                  />
+                  <Spacer y={1} />
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button
+                    auto
+                    rounded
+                    flat
+                    color="error"
+                    onPress={closeModalHandler}
+                  >
+                    Bezárás
+                  </Button>
+                  <Button auto rounded onPress={handleAddNewBrand}>
+                    Hozzáadás
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </>
+          )}
 
-        <div>
           <h4>Új típus hozzáadása</h4>
-          <Button size="md" rounded onPress={setAddNewTypeModalVisible}>
+          <Button
+            size="md"
+            className="car_data_add_new_button"
+            rounded
+            onPress={setAddNewTypeModalVisible}
+          >
             Hozzáadás
           </Button>
-        </div>
-        <Modal
-          closeButton
-          blur
-          aria-labelledby="modal-title"
-          open={addNewTypeModalVisible}
-          onClose={closeModalHandler}
-        >
-          <Modal.Header>
-            <Text id="modal-title" size={18}>
-              Új típus
-            </Text>
-          </Modal.Header>
-          <Modal.Body>
-            <Input
-              clearable
-              className={shakeTypeName ? "shake" : ""}
-              onChange={(e) => setTypeName(e.target.value)}
-              required
-              bordered
-              status={typeNameHelper.color}
-              color={typeNameHelper.color}
-              helperColor={typeNameHelper.color}
-              helperText={typeNameHelper.text}
-              fullWidth
-              size="md"
-              placeholder="Típus:"
-              aria-labelledby="Típus"
-            />
-            <Spacer y={1} />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button auto rounded flat color="error" onPress={closeModalHandler}>
-              Bezárás
-            </Button>
-            <Button auto rounded onPress={handleAddNewType}>
-              Hozzáadás
-            </Button>
-          </Modal.Footer>
-        </Modal>
-        <form onSubmit={handleSubmit} id="car_management_form">
-          <div>
-            <label>Márkanév:</label>
-            <select value={selectedBrand} onChange={handleBrandChange}>
-              <option value="">Válasszon márkát</option>
-              {brands.length > 0 ? (
-                brands.map((brand, index) => (
-                  <option key={index} value={brand.id}>
-                    {brand.brandName}
+
+          <Modal
+            closeButton
+            blur
+            aria-labelledby="modal-title"
+            open={addNewTypeModalVisible}
+            onClose={closeModalHandler}
+          >
+            <Modal.Header>
+              <Text id="modal-title" size={18}>
+                Új típus
+              </Text>
+            </Modal.Header>
+            <Modal.Body>
+              <Input
+                clearable
+                className={shakeTypeName ? "shake" : ""}
+                onChange={(e) => setTypeName(e.target.value)}
+                required
+                bordered
+                status={typeNameHelper.color}
+                color={typeNameHelper.color}
+                helperColor={typeNameHelper.color}
+                helperText={typeNameHelper.text}
+                fullWidth
+                size="md"
+                placeholder="Típus:"
+                aria-labelledby="Típus"
+              />
+              <Spacer y={1} />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                auto
+                rounded
+                flat
+                color="error"
+                onPress={closeModalHandler}
+              >
+                Bezárás
+              </Button>
+              <Button auto rounded onPress={handleAddNewType}>
+                Hozzáadás
+              </Button>
+            </Modal.Footer>
+          </Modal>
+          <form onSubmit={handleSubmit} id="car_management_form">
+            <div>
+              <Spacer y={2.5} />
+              <label className="car_data_form_select_label">Márkanév:</label>
+              <select value={selectedBrand} onChange={handleBrandChange}>
+                <option value="">Válasszon márkát</option>
+                {brands.length > 0 ? (
+                  brands.map((brand, index) => (
+                    <option key={index} value={brand.id}>
+                      {brand.brandName}
+                    </option>
+                  ))
+                ) : (
+                  <option value="">
+                    Nincs elérhető márka, kérjük vegye fel a kapcsolatot a
+                    support-tal
                   </option>
-                ))
-              ) : (
-                <option value="">
-                  Nincs elérhető márka, kérjük vegye fel a kapcsolatot a
-                  support-tal
-                </option>
-              )}
-            </select>
-          </div>
-          <div>
-            <label>Típus:</label>
+                )}
+              </select>
+            </div>
+            <Spacer y={2} />
+            <label className="car_data_form_select_label">Típus:</label>
             <select
               value={selectedType}
               onChange={(event) => handleTypeChange(event)}
@@ -310,41 +329,52 @@ function CarManagement() {
                   </option>
                 ))}
             </select>
-          </div>
-          <div>
-            <label>Évjárat:</label>
-            <input
+            <Spacer y={2} />
+            <Input
               type="number"
+              underlined
+              width="100%"
               value={year}
+              min="1885"
+              max={currentYear}
+              labelLeft="Évjárat"
               onChange={(e) => setYear(e.target.value)}
             />
-          </div>
-          <div>
-            <label>Szín:</label>
-            <input
+            <Spacer y={0.5} />
+            <Input
               type="text"
+              width="100%"
               value={color}
+              underlined
+              labelLeft="Szín"
+              label
               onChange={(e) => setColor(e.target.value)}
             />
-          </div>
-          <div>
-            <label>Üzemanyag típusa:</label>
-            <input
+            <Spacer y={2} />
+            <Input
+              style={{ fontSize: "" }}
               type="text"
               value={fuelType}
+              width="100%"
+              underlined
+              labelLeft="Üzemanyag típusa"
+              labelPlaceholder
               onChange={(e) => setFuelType(e.target.value)}
             />
-          </div>
-          <div>
-            <label>Images:</label>
-            <input
+            <Spacer y={2} />
+            <Input
               type="file"
+              width="100%"
+              underlined
+              labelLeft="Fotó feltöltése"
               onChange={(e) => setImages(e.target.files)}
               multiple
             />
-          </div>
-          <button type="submit">Save</button>
-        </form>
+            <Button id="car_data_submit_button" type="submit">
+              Mentés
+            </Button>
+          </form>
+        </div>
       </div>
     </>
   );
